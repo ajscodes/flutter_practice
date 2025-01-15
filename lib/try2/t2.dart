c1ba8922d94ec07e44278b86aa3102e4995e2fa1
ajscodes <ayushmaradia@gmail.com>
2025-01-15 21:05:02 +0530

    updating

--- lib/try2/t2.dart
+++ lib/try2/t2.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class Try1 extends StatelessWidget {
-  const Try1({super.key});
+class Try2 extends StatelessWidget {
+  const Try2({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("This is try1."),
+      body: Text("This is try2."),
     );
   }
 }
