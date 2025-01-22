c423c61dfd9805822f02b46e00b6295417cb9efc
ajscodes <ayushmaradia@gmail.com>
2025-01-22 22:03:03 +0530

    work in progress

---
+++ lib/Contact Diary/CRUD.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactDiary extends StatefulWidget {
+  const ContactDiary({super.key});
+
+  @override
+  State<ContactDiary> createState() => _ContactDiaryState();
+}
+
+class _ContactDiaryState extends State<ContactDiary> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
